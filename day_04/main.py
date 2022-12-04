c11a3f9910d09db60edf92959966fdb5cd47ed6f
Relate <blismatic@gmail.com>
2022-12-04 12:39:09 -0800

    refactor day_04

--- day_04/main.py
+++ day_04/main.py
@@ -2,6 +2,19 @@ def parse_raw():
     with open('input.txt') as f:
         data = f.read().strip().split('\n')
         data = [pair.split(',') for pair in data]
+        data = [[pair[0].split('-'), pair[1].split('-')] for pair in data]
+
+        for idx, pair in enumerate(data):
+            elf_1_start = int(pair[0][0])
+            elf_1_stop = int(pair[0][1])
+            elf_2_start = int(pair[1][0])
+            elf_2_stop = int(pair[1][1])
+
+            elf_1_range = range(elf_1_start, elf_1_stop + 1)
+            elf_2_range = range(elf_2_start, elf_2_stop + 1)
+
+            data[idx] = [set(elf_1_range), set(elf_2_range)]
+
     return data
 
 
@@ -10,33 +23,17 @@ data = parse_raw()
 
 def part_one():
     count = 0
-
-    for pairs in data:
-        elf_1 = pairs[0].split('-')
-        elf_2 = pairs[1].split('-')
-
-        range1 = set(range(int(elf_1[0]), int(elf_1[1]) + 1))
-        range2 = set(range(int(elf_2[0]), int(elf_2[1]) + 1))
-
-        if (range1.issubset(range2) or range2.issubset(range1)):
+    for pair in data:
+        if (pair[0].issubset(pair[1]) or pair[1].issubset(pair[0])):
             count += 1
-
     print(count)
 
 
 def part_two():
     count = 0
-
-    for pairs in data:
-        elf_1 = pairs[0].split('-')
-        elf_2 = pairs[1].split('-')
-
-        range1 = set(range(int(elf_1[0]), int(elf_1[1]) + 1))
-        range2 = set(range(int(elf_2[0]), int(elf_2[1]) + 1))
-
-        if (len(range1 & range2) > 0):
+    for pair in data:
+        if (len(pair[0] & pair[1]) > 0):
             count += 1
-
     print(count)
 
 
